c0358532263ee4604a2a70cae34f8477ce95666f
Aid0z <mutheleteric@gmail.com>
2020-09-28 21:40:37 +0200

    add 2nd player / add materials to reflect teams

--- Assets/Scripts/MovePlayers.cs
+++ Assets/Scripts/MovePlayers.cs
@@ -12,6 +12,23 @@ public class MovePlayers : MonoBehaviour
     public CharacterController controller;
     public float turnSmoothTime = 0.1f;
     private float turnSmoothVel;
+    
+    public enum ControllerType
+    {
+        AI, P1, P2
+    }
+
+    public ControllerType controllerType;
+    private Dictionary<ControllerType, KeyCode[]> inputs = new Dictionary<ControllerType,KeyCode[]>()
+    {
+        {ControllerType.P1, new []{KeyCode.Z, KeyCode.S, KeyCode.Q, KeyCode.D}},
+        {ControllerType.P2, new []{KeyCode.O, KeyCode.L, KeyCode.K, KeyCode.M}},
+    };
+    private Dictionary<ControllerType, string[]> axisNames = new Dictionary<ControllerType,string[]>()
+    {
+        {ControllerType.P1, new []{"Horizontal", "Vertical"}},
+        {ControllerType.P2, new []{"Horizontal2", "Vertical2"}},
+    };
 
     // Update is called once per frame
     void Update()
@@ -23,22 +40,22 @@ public class MovePlayers : MonoBehaviour
     {
         short keysCount = 0;
         
-        if (Input.GetKey(KeyCode.Z))
+        if (Input.GetKey(inputs[controllerType][0]))
         {
             animation.SetBool(Run,true);
             keysCount++;
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(inputs[controllerType][1]))
         {
             animation.SetBool(Run,true);
             keysCount++;
         }
-        if (Input.GetKey(KeyCode.Q))
+        if (Input.GetKey(inputs[controllerType][2]))
         {
             animation.SetBool(Run,true);
             keysCount++;
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(inputs[controllerType][3]))
         {
             animation.SetBool(Run,true);
             keysCount++;
@@ -57,8 +74,8 @@ public class MovePlayers : MonoBehaviour
     
     private void movePlayer()
     {
-        float MoveHorizontal = Input.GetAxisRaw("Horizontal");
-        float MoveVertical = Input.GetAxisRaw("Vertical");
+        float MoveHorizontal = Input.GetAxisRaw(axisNames[controllerType][0]);
+        float MoveVertical = Input.GetAxisRaw(axisNames[controllerType][1]);
         Vector3 direction = new Vector3(MoveHorizontal,0f,MoveVertical).normalized;
 
         if (direction.magnitude >= 0.1f)
